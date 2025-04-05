c21c78b51f8f311b38f9899464e78f77fac5dd1b
Wilma Moamba <wilmamoamba@gmail.com>
2025-04-06 05:28:41 +1400

    chatV1

---
+++ resources/views/telaPrincipal.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste com Tailwind</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js']) 
+    <!-- @vite('resources/css/app.css') -->
+</head>
+<body>
+    <div class="bg-blue-500 text-white p-4">
+        Olá, isso é um teste com Tailwind CSS!
+    </div>
+</body>
+</html>
+
+
